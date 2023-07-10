a52448a851ef7709c6651b15b6fb9b300366ae65
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-07-10 17:35:40 -0500

    add more design to todo

---
+++ src/main/resources/static/js/checkForLogin.js
@@ -0,0 +1,12 @@
+function checkUserId() {
+    const cookieArr = document.cookie.split(";");
+    const userId = cookieArr[0].split("=")[1];
+    if (userId !== undefined) {
+      window.location.replace("http://localhost:8080/itinerary.html");
+    }
+    console.log(userId);
+  }
+
+  // Check user ID on page load and page show
+  window.addEventListener("load", checkUserId);
+  window.addEventListener("pageshow", checkUserId);
