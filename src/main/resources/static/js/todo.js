dee15aa891505a62663cc3a35d1ccf686d96a941
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-07-15 12:56:32 -0500

    fixes todo

--- src/main/resources/static/js/todo.js
+++ src/main/resources/static/js/todo.js
@@ -174,13 +174,14 @@ function displayTodoSection(cards) {
   gsap.set(".card", { opacity: 0, y: 100 });
   ScrollTrigger.batch(".card", {
     onEnter: (batch) => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15 }),
-    onLeave: (batch) => gsap.to(batch, { opacity: 0, y: 100 }),
+    onLeave: (batch) => gsap.to(batch, { y: 100, immediateRender: false }),
     onEnterBack: (batch) => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15 }),
-    onLeaveBack: (batch) => gsap.to(batch, { opacity: 0, y: 100 }),
-
+    onLeaveBack: (batch) => gsap.to(batch, { y: 100, immediateRender: false }),
+  
     start: "top 90%",
     end: "bottom 10%",
   });
+  
 }
 
 function displayDoneSection(cards) {
